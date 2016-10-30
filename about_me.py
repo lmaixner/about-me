58a1eb85f764f99ca4ca47db4b2e4901c2bc68ca
Laura Maixner <maixnerla@mnstate.edu>
2016-10-30 16:21:00 -0500

    fixed Erin's line

--- about_me.py
+++ about_me.py
@@ -6,7 +6,7 @@ print("My name is Jane and I will not add punctuation to the end of this sentenc
 
 print("My name is Laura and I like twix.")
 
-print("My name is Erin and I am sad that all the chocolate candy is gone.   ")
+print("My name is Erin and I still have chocolate sitting next to me.")
 
 print("A girl has no name.")
 
